1ab5e1c26368a034f2e3261c91f2e691cb3a049e
hariom <hariom@gmail.com>
2023-12-14 10:26:14 +0530

    API compeleted

---
+++ DealerAPI/Models/RefreshToken.cs
@@ -0,0 +1,10 @@
+﻿namespace DealerAPI.Models
+{
+    public class RefreshToken
+    {
+        public string Token { get; set; } = string.Empty;
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime Expires { get; set; }
+
+    }
+}
